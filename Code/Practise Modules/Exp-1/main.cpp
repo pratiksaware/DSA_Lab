359d8721032941866b1cc0c77f6ca2950ef2f0fa
Varun More <morevarun4004@gmail.com>
2021-11-22 13:16:55 +0530

    Added Practise_Modules

---
+++ Code/Practise Modules/Exp-1/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<Code\Practise Modules\Exp-1\utilites\functionality.h>
+using namespace std;
+
+int main(){
+    student s[100] = student(45, "Varun", 9.9);
+    display();
+    
+    return 0;
+}
---
+++ Code/Practise Modules/Exp-1/utilites/functionality.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#pragma once
+#include<string>
+using namespace std;
+
+class student
+{
+    private:
+    
+    int roll_num;
+    string name;
+    float sgpa;
+    
+    
+    public:
+    student(int ROLL_NO, string NAME, float SGPA){
+        roll_num = ROLL_NO;
+        name = NAME;
+        sgpa = SGPA;
+    }
+
+    void display(){
+        cout<<"STUDENT NAME:\t"<< name<<"\t ROLL NO:\t"<<roll_num<<"\tSGPA:\t"<<sgpa;
+    }
+    void bubble_sort(){
+        
+    }
+};
