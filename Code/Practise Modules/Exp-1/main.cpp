8dcf934084ca8d54d687fb6ffae71c220973bd26
Varun More <morevarun4004@gmail.com>
2021-12-19 19:39:35 +0530

    Delete main.cpp

--- Code/Practise Modules/Exp-1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<E:\github repo's\DSA _Lab-Repo\Code\Practise Modules\Exp-1\utilites\functionality.h>
-using namespace std;
-
-int main(){
-    student s = student(56, "Varun", 9.4);
-
-    return 0;
-}
